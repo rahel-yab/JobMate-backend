fe689c18b7630999a8f2779aaa39e54c6a49825e
NaomiMeseret <naomimu2121@gmail.com>
2025-09-06 13:46:38 +0300

    Update security.go

--- Backend/infrastructure/middlewares/security.go
+++ Backend/infrastructure/middlewares/security.go
@@ -9,7 +9,7 @@ func SecurityHeaders() gin.HandlerFunc {
 		c.Writer.Header().Set("X-Frame-Options", "DENY")
 		c.Writer.Header().Set("X-XSS-Protection", "1; mode=block")
 		c.Writer.Header().Set("Referrer-Policy", "no-referrer")
-		c.Writer.Header().Set("Content-Security-Policy", "default-src 'self'")
+		c.Writer.Header().Set("Content-Security-Policy", "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:; connect-src *; img-src * data: blob:; media-src * data: blob:; frame-src *;")
 		c.Next()
 	}
 }
